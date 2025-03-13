07f9b4db8ec532f09425a9c7807551899a8adb24
adrtivv <adrtivv@gmail.com>
2025-03-13 10:12:47 +0530

    enable zed editor helix mode

--- users/adrtivv/home/zed-editor/default.nix
+++ users/adrtivv/home/zed-editor/default.nix
@@ -63,9 +63,9 @@
         mode = "system";
       };
       ui_font_size = 16;
-      # vim = {
-      #   default_mode = "helix_normal";
-      # };
+      vim = {
+        default_mode = "helix_normal";
+      };
       vim_mode = true;
     };
   };
