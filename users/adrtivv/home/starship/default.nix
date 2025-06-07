437243c0130cad125ee4cef1122625f36e7b7a58
adrtivv <adrtivv@gmail.com>
2025-06-07 15:30:57 +0530

    add starship.rs shell level

--- users/adrtivv/home/starship/default.nix
+++ users/adrtivv/home/starship/default.nix
@@ -2,5 +2,13 @@
   # https://home-manager-options.extranix.com/?query=programs.starship&release=master
   programs.starship = {
     enable = true;
+    settings = {
+      shlvl = {
+        disabled = false;
+        format = "\\[󰜴 $shlvl\\]($style) ";
+        style = "bold mauve";
+        threshold = 1;
+      };
+    };
   };
 }
