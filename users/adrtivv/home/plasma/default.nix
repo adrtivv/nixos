832675b9430bc6d27983648afba9c4e4385298da
adrtivv <adrtivv@gmail.com>
2025-06-29 21:22:33 +0530

    configure plasma

--- users/adrtivv/home/plasma/default.nix
+++ users/adrtivv/home/plasma/default.nix
@@ -5,6 +5,25 @@
       TerminalApplication = "ghostty";
       TerminalService = "com.mitchellh.ghostty.desktop";
     };
+    desktop = {
+      icons = {
+        alignment = "left";
+        arrangement = "topToBottom";
+        sorting = {
+          foldersFirst = false;
+          mode = "name";
+        };
+      };
+    };
     enable = true;
+    panels = [
+      {
+        alignment = "center";
+        floating = false;
+        hiding = "autohide";
+        lengthMode = "fill";
+        opacity = "adaptive";
+      }
+    ];
   };
 }
