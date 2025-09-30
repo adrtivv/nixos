a0c9597da6911b16321f886331c0e27317099aed
adrtivv <adrtivv@gmail.com>
2025-09-30 15:04:49 +0530

    disable easyeffects

--- users/adrtivv/home/default.nix
+++ users/adrtivv/home/default.nix
@@ -91,7 +91,7 @@
 
   # https://home-manager-options.extranix.com/?query=services.easyeffects&release=master
   services.easyeffects = {
-    enable = true;
+    enable = false;
     # preset = "";
   };
 
