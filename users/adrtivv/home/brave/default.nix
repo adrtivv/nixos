ca70554f56a528173b35bb7ee9ff4eabd48d7064
adrtivv <adrtivv@gmail.com>
2025-10-25 00:32:53 +0530

    enable missing brave touchpad gestures

--- users/adrtivv/home/brave/default.nix
+++ users/adrtivv/home/brave/default.nix
@@ -1,7 +1,21 @@
-{pkgs, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   home.packages = [
     # https://search.nixos.org/packages?channel=unstable&show=brave&query=brave
     # https://github.com/brave/brave-browser
-    pkgs.brave
+    (pkgs.brave.override
+      {
+        commandLineArgs = [
+          (lib.concatStringsSep "," [
+            "--enable-features=TouchpadOverscrollHistoryNavigation"
+            "AcceleratedVideoEncoder"
+          ])
+          "--enable-zero-copy"
+          "--ignore-gpu-blocklist"
+        ];
+      })
   ];
 }
