a2999494ffad852e682f99e02216c37e606c8d6a
adrtivv <adrtivv@gmail.com>
2026-02-03 15:13:17 +0530

    T_T

--- modules/_easy_effects.nix
+++ modules/_easy_effects.nix
@@ -8,7 +8,7 @@
       # https://home-manager-options.extranix.com/?query=services.easyeffects&release=master
       # https://github.com/wwmm/easyeffects
       services.easyeffects = {
-        enable = false;
+        enable = true;
       };
     };
 }
